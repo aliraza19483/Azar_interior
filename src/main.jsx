0ed4ef459c9d7eb243a59612ca6c755806653d96
aditiverma-glitch <20aditi.verma@gmail.com>
2026-02-12 15:22:30 +0530

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './header.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
     <Header/>
+    <Footer/>
   </>
 )
