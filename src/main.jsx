b953d7808056cb94ec3135e7a7c8138bce2384ab
Ali Raza <aliraza645.muz@gmail.com>
2026-02-15 02:01:41 +0530

    Create Hero Section

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import Hero from './components/Hero.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <>
     <Header />
+    <Hero/>
     <Footer />
   </>
 )
